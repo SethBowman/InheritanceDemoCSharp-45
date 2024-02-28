04797be20d498e3d876e6900701bf41bc6f28308
Seth Bowman <brandonsethbowman@gmail.com>
2024-02-28 14:19:09 -0600

    Add project files.

---
+++ InheritanceDemoCSharp-45/Coder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDemoCSharp_45
+{
+    public class Coder : Person
+    {
+        public string ProgrammingLang { get; set; }
+
+        public void Code()
+        {
+            Console.WriteLine($"I can code in {ProgrammingLang}!");
+        }
+    }
+}
---
+++ InheritanceDemoCSharp-45/Program.cs
@@ -0,0 +1,21 @@
+﻿using InheritanceDemoCSharp_45;
+
+var seth = new Person();
+
+seth.Name = "Seth";
+seth.Age = 29;
+
+seth.Greet();
+
+var mack = new Coder();
+
+mack.Name = "Mack";
+mack.ProgrammingLang = "C#";
+
+mack.Greet();
+mack.Code();
+
+var cruz = new FrontEndDev();
+
+cruz.Name = "Cruz";
+cruz.ProgrammingLang = "JavaScript";
